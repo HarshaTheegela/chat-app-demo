e4578140b302d4350a312a7cd5457b51d333b1f1
HarshaTheegela <chandraharsha.theegela@gmail.com>
2018-06-28 19:36:11 -0500

    modified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-./wait.sh mysql:3306 -- echo "mysql is up"
-if [$? -eq "0"]
-then
-   java -jar /target/chat-app-demo-0.0.1-SNAPSHOT.jar
-fi
+set -x
+./wait.sh mysql:3306 -- java -jar target/chat-app-demo-0.0.1-SNAPSHOT.jar
